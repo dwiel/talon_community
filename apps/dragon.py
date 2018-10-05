c61d59ed7d6db71c0d5eaa34c3a0175379466b10
Jen-Mei Wu <veganjenny@gmail.com>
2018-10-05 14:13:06 -0700

    Command to open dragonpad

---
+++ apps/dragon.py
@@ -0,0 +1,28 @@
+from talon.api import ffi
+from talon.voice import Context, Key
+from talon import ctrl, applescript
+import time
+
+def open_dragon_pad(m):
+    old = ctrl.mouse_pos()
+    x = applescript.run('''
+    tell application "System Events" to tell process "Dragon" to tell (menu bar item 1 of menu bar 2)
+        set AppleScript's text item delimiters to ", "
+        position as string
+    end tell
+    ''')
+    x, y = map(int, x.split(', '))
+    ctrl.mouse(x, y)
+    ctrl.mouse_click()
+    ctrl.mouse(*old)
+    applescript.run('''
+    tell application "System Events" to tell process "Dragon" to tell (menu bar item 1 of menu bar 2)
+        click menu item "Help" of menu 1
+        click menu item "DragonPad" of menu of menu item "Help" of menu 1
+    end tell
+    ''')
+
+ctx = Context('dragon')
+ctx.keymap({
+    'dragonpad': open_dragon_pad,
+})
