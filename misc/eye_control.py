59e9340714eb4c128f556a64d78b23a770b4c97d
Jen-Mei Wu <veganjenny@gmail.com>
2018-10-13 21:46:35 -0700

    Updated eye_control.py and speech_toggle.py for 0.0.8

--- misc/eye_control.py
+++ misc/eye_control.py
@@ -1,16 +1,11 @@
-import eye_mouse
-from talon.voice import Word, Context, Key, Rep, Str, press
+# From https://github.com/talonvoice/examples
+from talon_plugins import eye_mouse
+from talon.voice import Context
 
-ctx = Context("eye_control")
-ctx.keymap(
-    {
-        "debug overlay": lambda m: eye_mouse.on_menu(
-            "Eye Tracking >> Show Debug Overlay"
-        ),
-        "control mouse": lambda m: eye_mouse.on_menu("Eye Tracking >> Control Mouse"),
-        "camera overlay": lambda m: eye_mouse.on_menu(
-            "Eye Tracking >> Show Camera Overlay"
-        ),
-        "run calibration": lambda m: eye_mouse.on_menu("Eye Tracking >> Calibrate"),
-    }
-)
+ctx = Context('eye_control')
+ctx.keymap({
+    'debug overlay':   lambda m: eye_mouse.debug_overlay.toggle(),
+    'control mouse':   lambda m: eye_mouse.control_mouse.toggle(),
+    'camera overlay':  lambda m: eye_mouse.camera_overlay.toggle(),
+    'run calibration': lambda m: eye_mouse.calib_start(),
+})
--- misc/speech_toggle.py
+++ misc/speech_toggle.py
@@ -1,42 +1,16 @@
-from talon.api import lib
-from talon.voice import Context, ContextGroup, talon
+# From https://github.com/talonvoice/examples
+from talon.voice import Context, ContextGroup
 from talon.engine import engine
-from talon import app
+from talon_plugins import speech
 
+sleep_group = ContextGroup('sleepy')
+sleepy = Context('sleepy', group=sleep_group)
 
-def set_enabled(enable):
-    if enable:
-        talon.enable()
-        app.icon_color(0, 0.7, 0, 1)
-    else:
-        talon.disable()
-        app.icon_color(1, 0, 0, 1)
-    lib.menu_check(b"!Enable Speech Recognition", enable)
+sleepy.keymap({
+    'talon sleep': lambda m: speech.set_enabled(False),
+    'talon wake': lambda m: speech.set_enabled(True),
 
-
-def on_menu(item):
-    if item == "!Enable Speech Recognition":
-        set_enabled(not talon.enabled)
-
-
-app.register("menu", on_menu)
-set_enabled(talon.enabled)
-
-sleep_group = ContextGroup("sleepy")
-sleepy = Context("sleepy", group=sleep_group)
-
-sleepy.keymap(
-    {
-        "talon sleep": lambda m: set_enabled(False),
-        "talon wake": lambda m: set_enabled(True),
-        "dragon mode": [
-            lambda m: set_enabled(False),
-            lambda m: engine.mimic("wake up".split()),
-        ],
-        "talon mode": [
-            lambda m: set_enabled(True),
-            lambda m: engine.mimic("go to sleep".split()),
-        ],
-    }
-)
+    'dragon mode': [lambda m: speech.set_enabled(False), lambda m: engine.mimic('wake up'.split())],
+    'talon mode': [lambda m: speech.set_enabled(True), lambda m: engine.mimic('go to sleep'.split())],
+})
 sleep_group.load()
