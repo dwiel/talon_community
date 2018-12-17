3b6e0338c5d8a0e1ee11b4fea6cc88e08c3f48ba
Zach Dwiel <zach@dwiel.net>
2018-12-17 11:35:13 -0500

    and less verbose debug mode

--- misc/debug.py
+++ misc/debug.py
@@ -1,11 +1,20 @@
 from talon.engine import engine
 
+VERBOSE = False
 
 def listener(topic, m):
     if topic == "cmd" and m["cmd"]["cmd"] == "g.load" and m["success"] == True:
         print("[grammar reloaded]")
     else:
-        print(topic, m)
+        if VERBOSE:
+            print(topic, m)
+        elif topic == 'phrase' and 'words' in m and m['cmd'] != 'p.hypothesis':
+            try:
+                print(topic, m['phrase'], m['parsed'])
+            except:
+                print()
+                print(topic, m)
+                raise
 
 
 engine.register("", listener)
