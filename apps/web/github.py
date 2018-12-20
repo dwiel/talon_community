82b2a22308d3062e02dde777dff092eb90d70027
Nate O'Donnell <nodonnel@gmail.com>
2018-12-20 13:14:34 -0500

    added some shortcuts to github.py

--- apps/web/github.py
+++ apps/web/github.py
@@ -5,38 +5,61 @@ from talon import ctrl
 
 ctx = Context("github-sitewide", bundle="org.mozilla.firefox")
 
+# seconds to wait between keypresses
+lag = 0.3
+
 
 def normal_mode():
     # get out of any text box
-    press('escape')
-    time.sleep(0.1)
-    # make sure we are in normal mode
+    time.sleep(lag)
     press('escape')
 
 
 def page_mode():
     normal_mode()
-    time.sleep(0.1)
+    time.sleep(lag)
     # insert ignore mode
     press("ctrl-alt-escape")
 
 
 def search(m):
     page_mode()
+    time.sleep(lag)
     press('/')
 
 
-def notifications(m):
+def goto_notifications(m):
     page_mode()
-    time.sleep(0.1)
+    time.sleep(lag)
     press('g')
-    time.sleep(0.1)
+    time.sleep(lag)
     press('n')
+
+
+def goto_code(m):
+    page_mode()
+    time.sleep(lag)
+    press('g')
+    time.sleep(lag)
+    press('c')
+
+
+def goto_issues(m):
+    page_mode()
+    time.sleep(lag)
+    press('g')
+    time.sleep(lag)
+    press('i')
 
 
 ctx.keymap(
     {
         'jet search': search,
-        'notes': notifications
+        '(notes | notifications)': goto_notifications,
+        # '(hover)': hover
+
+        # Reposity shortcuts; need to be moved into different file
+        '[go to] code': goto_code,
+        '[go to] issues': goto_issues
     }
 )
