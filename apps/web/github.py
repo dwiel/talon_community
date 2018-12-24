c0c6774d71d240594b35099e67ab9b69b64a9998
Nate O'Donnell <nodonnel@gmail.com>
2018-12-24 14:01:20 -0500

    updated github.py

--- apps/web/github.py
+++ apps/web/github.py
@@ -6,81 +6,248 @@ from talon import ctrl
 ctx_global = Context("github-sitewide", bundle="org.mozilla.firefox")
 ctx_repo = Context("github-repo",
                    func=lambda app, win: win.title.startswith("dwiel"))
+ctx_editor = Context("github-code-editor",
+                     func=lambda app, win: win.title.startswith("Editing"))
+ctx_issues_pull_lists = Context("github-issues-pull-requests_lists",
+                                func=lambda app, win:
+                                win.title.startswith("Issues") or
+                                win.title.startswith("Pull Requests"))
+ctx_issues_pull = Context("github-issues-pull-requests",
+                          func=lambda app, win:
+                          win.title.find(" Issue ") or
+                          win.title.find(" Pull Request "))
+ctx_pull_changes = Context("github-pull-request-changes",
+                           func=lambda app, win:
+                           win.title.find(" Pull Request "))
+ctx_network_graph = Context("github-network-graph",
+                            func=lambda app, win:
+                            win.title.startswith("Network Graph"))
+
+# Source: https://help.github.com/articles/using-keyboard-shortcuts/
+#   To do:
+#       * Contexts & shorcuts for:
+#            * Source code browsing
+#            * Comments (markdown)
+#            * Project boards
+#            * Notifications
+#       * Make global (firefox.py) "page mode" and "tridactyl mode" rules
+#       * Test rule for "Add a single comment on a pull request diff"
+#
+#   Issues:
+#       * On Issues/Pull Requests List Pages, "cmd-/"
+#         conflicts w/ Firefox shortcuts
+#       * Need a better way to define "github-repo" context
 
 
-# seconds to wait between keypresses
-lag = 0.3
+lag = 0.2
+using_tridactyl = True
 
 
 def normal_mode():
     # get out of any text box
     time.sleep(lag)
     press('escape')
+    time.sleep(lag)
+
+
+def tridactyl_mode():
+    if using_tridactyl:
+        # normal_mode()
+        time.sleep(lag)
+        press("ctrl-alt-escape")
+        time.sleep(lag)
 
 
 def page_mode():
     normal_mode()
-    time.sleep(lag)
-    # insert ignore mode
-    press("ctrl-alt-escape")
+    if using_tridactyl:
+        press("ctrl-alt-escape")
+        time.sleep(lag)
 
 
-# GLOBAL METHODS
+# SITE-WIDE METHODS
 
 def search(m):
     page_mode()
-    time.sleep(lag)
     press('/')
 
 
 def goto_notifications(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('n')
+    tridactyl_mode()
 
 # REPO METHODS
 
 
 def repo_goto_code(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('c')
+    tridactyl_mode()
 
 
 def repo_goto_issues(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('i')
+    tridactyl_mode()
 
 
 def repo_goto_pull_requests(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('p')
+    tridactyl_mode()
 
 
 def repo_goto_projects(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('b')
+    tridactyl_mode()
 
 
 def repo_goto_wiki(m):
     page_mode()
-    time.sleep(lag)
     press('g')
     time.sleep(lag)
     press('w')
+    tridactyl_mode()
+
+
+def repo_find_file(m):
+    page_mode()
+    press('t')
+
+
+def repo_switch_branch(m):
+    page_mode()
+    press('w')
+
+# ISSUES AND PULL REQUESTS LISTS METHODS
+
+
+def create_issue(m):
+    page_mode()
+    press('c')
+
+
+def filter_by_author(m):
+    page_mode()
+    press('u')
+
+
+def filter_by_label(m):
+    page_mode()
+    press('l')
+
+
+def filter_by_milestone(m):
+    page_mode()
+    press('m')
+
+
+def filter_by_assignee(m):
+    page_mode()
+    press('a')
+
+
+def open_issue(m):
+    page_mode()
+    press('o')
+    tridactyl_mode()
+
+
+# ISSUES AND PULL REQUESTS METHODS
+
+
+def request_reviewer(m):
+    page_mode()
+    press('q')
+
+
+def set_milestone(m):
+    page_mode()
+    press('m')
+
+
+def apply_label(m):
+    page_mode()
+    press('l')
+
+
+def set_assignee(m):
+    page_mode()
+    press('a')
+
+
+# PULL REQUEST CHANGES METHODS
+
+def list_commits(m):
+    page_mode()
+    press('c')
+
+
+def list_changed_files(m):
+    page_mode()
+    press('t')
+
+# NETWORK GRAPH METHODS
+
+
+def scroll_left(m):
+    page_mode()
+    press('h')
+    tridactyl_mode()
+
+
+def scroll_right(m):
+    page_mode()
+    press('l')
+    tridactyl_mode()
+
+
+def scroll_down(m):
+    page_mode()
+    press('j')
+    tridactyl_mode()
+
+
+def scroll_up(m):
+    page_mode()
+    press('k')
+    tridactyl_mode()
+
+
+def scroll_left_most(m):
+    page_mode()
+    press('shift-h')
+    tridactyl_mode()
+
+
+def scroll_right_most(m):
+    page_mode()
+    press('shift-l')
+    tridactyl_mode()
+
+
+def scroll_down_most(m):
+    page_mode()
+    press('shift-j')
+    tridactyl_mode()
+
+
+def scroll_up_most(m):
+    page_mode()
+    press('shift-k')
+    tridactyl_mode()
 
 
 ctx_global.keymap(
@@ -88,9 +255,6 @@ ctx_global.keymap(
         'jet search': search,
         '(notes | notifications)': goto_notifications,
         # '(hover)': hover
-
-        # Reposity shortcuts; need to be moved into different file
-
     }
 )
 
@@ -100,6 +264,64 @@ ctx_repo.keymap(
         '[go to] issues': repo_goto_issues,
         '[go to] (pull | pulls)[requests]': repo_goto_pull_requests,
         '[go to] projects': repo_goto_projects,
-        '[go to] wiki': repo_goto_wiki
+        '[go to] wiki': repo_goto_wiki,
+        'find [file]': repo_find_file,
+        'switch [(branch | tag)]': repo_switch_branch
+    }
+)
+
+ctx_editor.keymap(
+    {
+        '(search | find)': Key('cmd-f'),
+        '[find] next': Key('cmd-g'),
+        '[find] prev': Key('shift-cmd-g'),
+        'replace': Key('cmd-alt-f'),
+        'replace all': Key('shift-cmd-alt-f'),
+        '(go to | jump to) [line]': Key('alt-g'),
+        'undo': Key('cmd-z'),
+        'redo': Key('cmd-y')
+    }
+)
+
+ctx_issues_pull_lists.keymap(
+    {
+        'create [issue] [pull request]': create_issue,
+        '[filter] [by] author': filter_by_author,
+        '[filter] [by] label': filter_by_label,
+        '[filter] [by] milestone': filter_by_milestone,
+        '[filter] [by] [(worker | assigned | assignee)]': filter_by_assignee,
+        'open [issue] [pull request]': open_issue
+    }
+)
+
+ctx_issues_pull.keymap(
+    {
+        '[request] (reviewer | review)': request_reviewer,
+        '[set] milestone': set_milestone,
+        '[apply] label': apply_label,
+        'assign': set_assignee
+    }
+)
+
+ctx_pull_changes.keymap(
+    {
+        '[list] commits': list_commits,
+        '[list] [changed] files': list_changed_files,
+        'down': Key('j'),
+        'up': Key('k'),
+        '[add] [diff] comment': Key('cmd-shift-enter')
+    }
+)
+
+ctx_network_graph.keymap(
+    {
+        '[scroll] left': scroll_left,
+        '[scroll] right': scroll_right,
+        '[scroll] down': scroll_down,
+        '[scroll] up': scroll_up,
+        '[scroll] left most': scroll_left_most,
+        '[scroll] right most': scroll_right_most,
+        '[scroll] down most': scroll_down_most,
+        '[scroll] up most': scroll_up_most,
     }
 )
