b1e518ba7e31a3305f8fa5680c103a6ebebb46cb
Dylan Chong <dylan.chong@icloud.com>
2018-12-31 16:05:35 +1300

    Add note to basic_keys about no modifiers in symbols dict

--- misc/basic_keys.py
+++ misc/basic_keys.py
@@ -11,8 +11,6 @@ simple_keys = [
     'home', 'pageup', 'pagedown', 'end',
 ]
 alternate_keys = {
-
-
     'backspace': 'backspace',
     'delete': 'backspace',
     'forward delete': 'delete',
@@ -27,6 +25,10 @@ alternate_keys = {
     'shock': 'enter',
 }
 symbols = {
+    # NOTE:  This should only contain symbols that do not require any modifier
+    # keys to press on a standard US keyboard layout. Commands for keys that do
+    # require modifiers (e.g. ``"caret": "^"`) should belong in
+    # ``text/symbol.py``.
     'back tick': '`', 'tick': '`',
     'comma': ',',
     'dot': '.', 'period': '.',
