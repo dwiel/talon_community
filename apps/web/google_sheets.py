5681213607080ece69d1696127734d6ce101f7f3
Zach Dwiel <zdwiel@gmail.com>
2019-01-14 21:29:55 -0500

    flush out google sheets more

--- apps/web/google_sheets.py
+++ apps/web/google_sheets.py
@@ -5,8 +5,9 @@ import time
 from talon.voice import Context, Key, Str, press
 from talon import ctrl, clip
 
-from ...utils import select_single, numerals, delay
-from ...misc.basic_keys import get_keys
+from ...utils import select_single, numerals, delay, preserve_clipboard
+from ...misc.basic_keys import get_keys, alphabet
+from ...misc import basic_keys
 
 ctx = Context(
     "google_sheets",
@@ -15,6 +16,7 @@ ctx = Context(
 )
 
 
+@preserve_clipboard
 def get_url():
     # TODO: retrieve url in a more direct way
     press("cmd-l")
@@ -24,21 +26,26 @@ def get_url():
     return clip.get()
 
 
-def update_range(column, row):
-    url = get_url()
-    print(("url", url))
-
-    # TODO: handle malformed URL (including the case where there is gibberish in the address bar)
-    updated_url = update_query_parameters(url, {"range": r"%s%s" % (column, row)})
-    print(("updated_url", updated_url))
-
+@preserve_clipboard
+def set_url(url):
     # update the address bar with the updated URL
-    clip.set(updated_url)
+    clip.set(url)
     time.sleep(0.25)
     press("cmd-v")
 
     # navigate to new URL
     press("enter")
+
+
+def update_selected_cell(column, row):
+    url = get_url()
+    updated_url = update_query_parameters(url, {"range": "%s%s%s%s" % (column, row)})
+    set_url(updated_url)
+
+def update_selected_cells(column, row, dest_column, dest_row):
+    url = get_url()
+    updated_url = update_query_parameters(url, {"range": "%s%s:%s%s" % (column, row, dest_column, dest_row)})
+    set_url(updated_url)
 
 
 def update_query_parameters(url, parameters):
@@ -47,6 +54,7 @@ def update_query_parameters(url, parameters):
     query.update(parameters)
 
     url_parts[4] = urllib.parse.urlencode(query)
+    url_parts[4] = url_parts[4].replace("%3A", ":")
 
     # some manual modification because urllib insists on putting the fragment identifier after the query parameters, which is not what we want here
     fragment = url_parts[5]
@@ -59,26 +67,34 @@ def update_query_parameters(url, parameters):
 
 
 # Talon Sheets command handlers
-def go_to_cell():
-    def _go_to_cell(m):
-        # TODO: support columns with multiple characters: AC
-        column = "".join(get_keys(m))
-        row = str(m._words[2])
-        update_range(column, row)
-
-    return _go_to_cell
+def go_to_cell(m):
+    column = "".join(get_keys(m))
+    row = "".join(map(str, m._words[1 + len(column) :]))
+    update_selected_cell(column, row)
 
 
-def go_to_named_cell():
-    def _go_to_named_cell(m):
-        # TODO: how to more easily express intended # of words in command?
-        region = str(m._words[1])
-        cell = region_map[region]
+def go_to_named_cell(m):
+    # TODO: how to more easily express intended # of words in command?
+    region = str(m._words[1])
+    cell = region_map[region]
 
-        update_range(*cell)
+    update_selected_cell(*cell)
 
-    return _go_to_named_cell
 
+def select_cells(m):
+    range = {0: {"column": "", "row": ""}, 1: {"column": "", "row": ""}}
+
+    i = 0
+    for word in m._words:
+        if word in basic_keys.alphabet.keys():
+            if range[i]["row"]:
+                i += 1
+
+            range[i]["column"] += basic_keys.alphabet[str(word)]
+        elif str(word) in basic_keys.digits:
+            range[i]["row"] += str(word)
+
+    update_selected_cells(range[0]["column"], range[0]["row"], range[1]["column"], range[1]["row"])
 
 ## key can only be a single word for now
 # TODO: load this from a json file
@@ -95,12 +111,13 @@ cells = "{basic_keys.alphabet}+" + numerals
 ctx.keymap(
     {
         # navigation by cell reference
-        "(view | go to | spring) " + cells: go_to_cell(),
-        "edit " + cells: [go_to_cell(), delay(1.0), Key("enter")],
+        "(view | go to | spring) " + cells: go_to_cell,
+        "edit " + cells: [go_to_cell, delay(1.0), Key("enter")],
         # navigation by name, specified in map above (cell column names, e.g. A-Z, not allowed to avoid conflicting with above cell references)
-        "(view | go to | spring) " + regions: go_to_named_cell(),
-        "edit " + regions: [go_to_named_cell(), delay(1.0), Key("enter")],
+        "(view | go to | spring) " + regions: go_to_named_cell,
+        "edit " + regions: [go_to_named_cell, delay(1.0), Key("enter")],
         # keyboard shortcut mappings
+        "(select range | selrang)" + cells + cells: select_cells,
         "select column": Key("ctrl+space"),
         "select row": Key("shift+space"),
         "select all": Key("cmd+a"),
@@ -139,7 +156,7 @@ ctx.keymap(
         "insert time": Key("cmd+shift+;"),
         "insert date": Key("cmd+;"),
         "insert date and time": Key("cmd+alt+shift+;"),
-        "format as decimal": Key("ctrl+shift+1"),
+        "format as (decimal | number)": Key("ctrl+shift+1"),
         "format as time": Key("ctrl+shift+2"),
         "format as date": Key("ctrl+shift+3"),
         "format as currency": Key("ctrl+shift+4"),
@@ -150,12 +167,27 @@ ctx.keymap(
         "move to beginning of sheet": Key("cmd+home"),
         "move to end of row": Key("end"),
         "move to end of sheet": Key("cmd+end"),
+        "sheet move left": [Key("alt-shift-s"), Key("up"), Key("enter")],
+        "sheet move right": [Key("alt-shift-s"), Key("up"), Key("up"), Key("enter")],
         "scroll to active cell": Key("cmd+backspace"),
         "(move to next sheet | sheet next)": Key("cmd+shift+pagedown"),
-        "(move to previous sheet | sheet previous | sheet prev)": Key("cmd+shift+pageup"),
+        "(move to previous sheet | sheet previous | sheet prev)": Key(
+            "cmd+shift+pageup"
+        ),
         "(display list of sheets | sheet list)": Key("alt+shift+k"),
-        "sheet new": Key('shift-f11'),
-        "sheet rename": [Key('alt-shift-s'), Key('down'), delay(), Key('down'), delay(), Key('down'), delay(), Key('down'), delay(), Key('enter')],
+        "sheet new": Key("shift-f11"),
+        "sheet rename": [
+            Key("alt-shift-s"),
+            Key("down"),
+            delay(),
+            Key("down"),
+            delay(),
+            Key("down"),
+            delay(),
+            Key("down"),
+            delay(),
+            Key("enter"),
+        ],
         "open hyperlink": Key("alt+enter"),
         "open explore": Key("alt+shift+x"),
         "go to side panel": Key("cmd+alt+."),
@@ -192,6 +224,18 @@ ctx.keymap(
             press("c"),
             ctrl.key_press("cmd", ctrl=True, cmd=True, up=True),
         ),
+        "decrease decimal places": [
+            Key("alt-/"),
+            "decrease decimal places",
+            delay(),
+            Key("enter"),
+        ],
+        "increase decimal places": [
+            Key("alt-/"),
+            "increase decimal places",
+            delay(),
+            Key("enter"),
+        ],
         "file menu": Key("ctrl+alt+f"),
         "edit menu": Key("ctrl+alt+e"),
         "view menu": Key("ctrl+alt+v"),
@@ -209,8 +253,8 @@ ctx.keymap(
         "context menu": Key("cmd+shift+\\"),
         "insert (rows | row) above": Key("ctrl+alt+i r"),
         "insert (rows | row) below": Key("ctrl+alt+i b"),
-        "insert (columns | column) to the left": Key("ctrl-alt-i c"),
-        "insert (columns | column) to the right": Key("ctrl+alt+i o"),
+        "insert (columns | column) [to the] left": Key("ctrl-alt-i c"),
+        "insert (columns | column) [to the] right": Key("ctrl+alt+i o"),
         "delete (rows | row)": Key("ctrl+alt+e d"),
         "delete (columns | column)": Key("cmd+alt+-"),
         "hide row": Key("cmd+alt+9"),
@@ -231,5 +275,15 @@ ctx.keymap(
         "turn on screen reader support": Key("cmd+alt+z"),
         "read column": Key("cmd+alt+shift+c"),
         "read row": Key("cmd+alt+shift+r"),
+        "(resize column | column resize)": [
+            Key("up"),
+            Key("down"),
+            Key("ctrl+space"),
+            Key("ctrl+space"),
+            Key("cmd+shift+\\"),
+            delay(),
+        ]
+        + [Key("down")] * 9
+        + [Key("enter")],
     }
 )
