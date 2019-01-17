e0ca016a9c48728229dbc78dc42968a32b9fc78b
Naomi Saphra <nsaphra@gmail.com>
2019-01-17 15:42:27 +0000

    Add dictation mode

--- misc/speech_toggle.py
+++ misc/speech_toggle.py
@@ -1,4 +1,3 @@
-# From https://github.com/talonvoice/examples
 from talon.voice import Context, ContextGroup
 from talon.engine import engine
 from talon_plugins import speech
@@ -6,11 +5,18 @@ from talon_plugins import speech
 sleep_group = ContextGroup('sleepy')
 sleepy = Context('sleepy', group=sleep_group)
 
+dictation_group = ContextGroup('dictation')
+dictation = Context('dictation', group=dictation_group)
+dictation_group.load()
+dictation_group.disable()
+
 sleepy.keymap({
     'talon sleep': lambda m: speech.set_enabled(False),
     'talon wake': lambda m: speech.set_enabled(True),
 
-    'dragon mode': [lambda m: speech.set_enabled(False), lambda m: engine.mimic('wake up'.split())],
-    'talon mode': [lambda m: speech.set_enabled(True), lambda m: engine.mimic('go to sleep'.split())],
+    'dragon mode': [lambda m: speech.set_enabled(False), lambda m: dictation_group.disable(), lambda m: engine.mimic('wake up'.split())],
+    'dictation mode': [lambda m: speech.set_enabled(False), lambda m: engine.mimic('go to sleep'.split()), lambda m: dictation_group.enable()],
+    'talon mode': [lambda m: speech.set_enabled(True), lambda m: dictation_group.disable(), lambda m: engine.mimic('go to sleep'.split())],
+    'full sleep mode': [lambda m: speech.set_enabled(False), lambda m: dictation_group.disable(), lambda m: engine.mimic('go to sleep'.split())]
 })
 sleep_group.load()
