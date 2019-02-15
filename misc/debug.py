aea692b7ad62330e65dac74c51c7710888acf744
Samuel Galson <samgalson@gmail.com>
2019-02-15 20:37:24 +0000

    add logging options to debug.py

--- misc/debug.py
+++ misc/debug.py
@@ -1,20 +1,30 @@
 from talon.engine import engine
+from talon import ui, voice
 
-VERBOSE = False
+CRAZY = False
+VERBOSE = True
+UI_EVENTS = False
 
 def listener(topic, m):
     if topic == "cmd" and m["cmd"]["cmd"] == "g.load" and m["success"] == True:
         print("[grammar reloaded]")
     else:
-        if VERBOSE:
+        if CRAZY:
+            print(topic, m)
+        elif VERBOSE and topic == "cmd" and m["cmd"]["cmd"] != "g.listset":
             print(topic, m)
         elif topic == 'phrase' and 'words' in m and m['cmd'] != 'p.hypothesis':
             try:
                 print(topic, m['phrase'], m['parsed'])
             except:
-                print()
                 print(topic, m)
-                raise
 
 
 engine.register("", listener)
+
+def ui_event(event, arg):
+        contexts = [ctx.name for ctx in voice.talon.active]
+        print('ui_event', {'event': event, 'arg': arg, 'active': ui.active_app(), 'contexts': contexts})
+
+if UI_EVENTS:
+    ui.register('', ui_event)
