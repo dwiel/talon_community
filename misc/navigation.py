937d93754f5090a9c1ff6e2e85302d2bf8d32d33
Samuel Galson <samgalson@gmail.com>
2019-02-25 14:08:24 +0000

    use cmd down/up for scrolling

--- misc/navigation.py
+++ misc/navigation.py
@@ -7,8 +7,8 @@ keymap = {
     "scroll down": [Key("down")] * 30,
     "scroll up": [Key("up")] * 30,
     # NB home and end do not work in all apps
-    "(scroll way down | doomway)": [Key("pageup")] * 30,
-    "(scroll way up | jeepway)": [Key("pagedown")] * 30,
+    "(scroll way down | doomway)": Key("cmd-down"),
+    "(scroll way up | jeepway)": Key("cmd-up"),
     "page up": [Key("pageup")],
     "page down": [Key("pagedown")],
     # searching
