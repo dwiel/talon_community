c861f5ff1abb74565bcff3cbfa289664f0b1d0eb
Samuel Galson <samgalson@gmail.com>
2019-02-25 15:34:55 +0000

    fix basic_keys

--- misc/basic_keys.py
+++ misc/basic_keys.py
@@ -1,53 +1,65 @@
 from talon.voice import Context, Str, press
 import string
 
-alpha_alt = 'air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip'.split()
+alpha_alt = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split()
 
-f_keys = {f'F {i}': f'f{i}' for i in range(1, 13)}
+f_keys = {f"F {i}": f"f{i}" for i in range(1, 13)}
 # arrows are separated because 'up' has a high false positive rate
-arrows = ['left', 'right', 'up', 'down']
-simple_keys = [
-    'tab', 'escape', 'enter', 'space',
-    'home', 'pageup', 'pagedown', 'end',
-]
+arrows = ["left", "right", "up", "down"]
+simple_keys = ["tab", "escape", "enter", "space", "home", "pageup", "pagedown", "end"]
 alternate_keys = {
-    'backspace': 'backspace',
-    'delete': 'backspace',
-    'forward delete': 'delete',
-
+    "backspace": "backspace",
+    "delete": "backspace",
+    "forward delete": "delete",
     # voicecode alternates
-    'tarp': 'tab',
-    '(crimp | lloyd )': 'left',
-    'chris': 'right',
-    'jeep': 'up',
-    '(down | dune | doom)': 'down',
-    'junk': 'backspace',
-    'shock': 'enter',
-    'scrap': 'delete',
+    "tarp": "tab",
+    "crimp": "left",
+    "lloyd": "left",
+    "chris": "right",
+    "jeep": "up",
+    "down": "down",
+    "dune": "down",
+    "doom": "down",
+    "junk": "backspace",
+    "shock": "enter",
+    "scrap": "delete",
+    "spunk": "delete",
+    "randall": "esc",
 }
 symbols = {
     # NOTE:  This should only contain symbols that do not require any modifier
     # keys to press on a standard US keyboard layout. Commands for keys that do
     # require modifiers (e.g. ``"caret": "^"`) should belong in
     # ``text/symbol.py``.
-    'back tick': '`', 'tick': '`',
-    ',': ',',
-    'dot': '.', 'period': '.',
-    'semi': ';', 'semicolon': ';',
-    'quote': "'",
-    'L square': '[', 'left square': '[', 'left square bracket': '[', 'square': '[',
-    'R square': ']', 'right square': ']', 'right square bracket': ']',
-    'forward slash': '/', 'slash': '/',
-    'backslash': '\\',
-    'minus': '-', 'dash': '-',
-    'equals': '=',
+    "back tick": "`",
+    "tick": "`",
+    ",": ",",
+    "comma": ",",
+    "dot": ".",
+    "period": ".",
+    "semi": ";",
+    "semicolon": ";",
+    "quote": "'",
+    "L square": "[",
+    "left square": "[",
+    "left square bracket": "[",
+    "square": "[",
+    "R square": "]",
+    "right square": "]",
+    "right square bracket": "]",
+    "forward slash": "/",
+    "slash": "/",
+    "backslash": "\\",
+    "minus": "-",
+    "dash": "-",
+    "equals": "=",
 }
 modifiers = {
-    'command': 'cmd',
-    'control': 'ctrl',
-    'shift': 'shift',
-    'alt': 'alt',
-    'option': 'alt',
+    "command": "cmd",
+    "control": "ctrl",
+    "shift": "shift",
+    "alt": "alt",
+    "option": "alt",
 }
 
 alphabet = dict(zip(alpha_alt, string.ascii_lowercase))
@@ -66,46 +78,61 @@ keymap.update(arrows)
 keymap.update(alphabet)
 keymap.update(digits)
 
+
 def insert(s):
     Str(s)(None)
 
+
 def get_modifiers(m):
     try:
-        return [modifiers[mod] for mod in m['basic_keys.modifiers']]
+        return [modifiers[mod] for mod in m["basic_keys.modifiers"]]
     except KeyError:
         return []
 
+
 def get_keys(m):
-    groups = ['basic_keys.keys', 'basic_keys.arrows', 'basic_keys.digits', 'basic_keys.alphabet', 'basic_keys.keymap']
+    groups = [
+        "basic_keys.keys",
+        "basic_keys.arrows",
+        "basic_keys.digits",
+        "basic_keys.alphabet",
+        "basic_keys.keymap",
+    ]
     for group in groups:
         try:
             return [keymap[k] for k in m[group]]
-        except KeyError: pass
+        except KeyError:
+            pass
     return []
 
+
 def uppercase_letters(m):
-    insert(''.join(get_keys(m)).upper())
+    insert("".join(get_keys(m)).upper())
+
 
 def press_keys(m):
     mods = get_modifiers(m)
     keys = get_keys(m)
     if mods:
-        press('-'.join(mods + [keys[0]]))
+        press("-".join(mods + [keys[0]]))
         keys = keys[1:]
     for k in keys:
         press(k)
 
-ctx = Context('basic_keys')
-ctx.keymap({
-    '(uppercase | ship | sky) {basic_keys.alphabet}+ [(lowercase | sunk)]': uppercase_letters,
-    '{basic_keys.modifiers}* {basic_keys.alphabet}+': press_keys,
-    '{basic_keys.modifiers}* {basic_keys.digits}+': press_keys,
-    '{basic_keys.modifiers}* {basic_keys.keys}+': press_keys,
-    '(go | {basic_keys.modifiers}+) {basic_keys.arrows}+': press_keys,
-})
-ctx.set_list('alphabet', alphabet.keys())
-ctx.set_list('arrows', arrows.keys())
-ctx.set_list('digits', digits.keys())
-ctx.set_list('keys', keys.keys())
-ctx.set_list('modifiers', modifiers.keys())
-ctx.set_list('keymap', keymap.keys())
+
+ctx = Context("basic_keys")
+ctx.keymap(
+    {
+        "(uppercase | ship | sky) {basic_keys.alphabet}+ [(lowercase | sunk)]": uppercase_letters,
+        "{basic_keys.modifiers}* {basic_keys.alphabet}+": press_keys,
+        "{basic_keys.modifiers}* {basic_keys.digits}+": press_keys,
+        "{basic_keys.modifiers}* {basic_keys.keys}+": press_keys,
+        "(go | {basic_keys.modifiers}+) {basic_keys.arrows}+": press_keys,
+    }
+)
+ctx.set_list("alphabet", alphabet.keys())
+ctx.set_list("arrows", arrows.keys())
+ctx.set_list("digits", digits.keys())
+ctx.set_list("keys", keys.keys())
+ctx.set_list("modifiers", modifiers.keys())
+ctx.set_list("keymap", keymap.keys())
