a892f5fd268e16143eeb46e40cebff31db3f1af3
Zach Dwiel <zdwiel@gmail.com>
2019-03-19 08:03:10 -0400

    talon_editor: 'press {modifiers}* {keymap}'

--- lang/talon_editor.py
+++ lang/talon_editor.py
@@ -11,13 +11,17 @@ FILETYPES = (".py",)
 ctx = Context("talon_editor", func=is_filetype(FILETYPES))
 
 
-def key(m):
-    modifiers = basic_keys.get_modifiers(m)
-    key = basic_keys.get_keys(m)[0]
-    if key is None:
-        print("no key", m)
-        return
-    Str("Key('{}')".format("-".join(modifiers + [key])))(None)
+def make_key(function_name="Key"):
+    def key(m):
+        modifiers = basic_keys.get_modifiers(m)
+        key = basic_keys.get_keys(m)[0]
+        if key is None:
+            print("no key", m)
+            return
+        key_text = "-".join(modifiers + [key])
+        Str(f"{function_name}('{key_text}')")(None)
+
+    return key
 
 
 def format_text(fmt):
@@ -42,7 +46,8 @@ def add_alternative(m):
 
 ctx.keymap(
     {
-        "key {basic_keys.modifiers}* {basic_keys.keymap}": key,
+        "key {basic_keys.modifiers}* {basic_keys.keymap}": make_key("Key"),
+        "press {basic_keys.modifiers}* {basic_keys.keymap}": make_key("press"),
         "talon map <dgndictation>": ("'", text, "': ,", Key("left")),
         "talon map string <dgndictation>": format_text("'{0}': '{0}',"),
         "dragon dictation": "<dgndictation>",
