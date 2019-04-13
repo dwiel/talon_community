a5169c06714532aaacc097d79b3b6c5d142ebe9d
Kevin Kaland <admin@wizonesolutions.com>
2019-04-13 12:57:05 +0200

    Add easy pane navigation.

--- apps/iterm.py
+++ apps/iterm.py
@@ -4,9 +4,13 @@ ctx = Context("iterm", bundle="com.googlecode.iterm2")
 
 keymap = {
     "broadcaster": Key("cmd-alt-i"),
+    "password": Key("cmd-alt-f"),
+
+    # Pane creation and navigation
     "split horizontal": Key("cmd-shift-d"),
     "split vertical": Key("cmd-d"),
-    "password": Key("cmd-alt-f"),
+    "pane next": Key("cmd-]"),
+    "pane last": Key("cmd-["),
 }
 
 ctx.keymap(keymap)
