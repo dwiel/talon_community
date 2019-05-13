a83ea5b6bb74e86675d5cd8e3e2ce73cd1c500b3
Zach Dwiel <zdwiel@gmail.com>
2019-05-12 21:36:11 -0400

    amethyst: remove `window move desk` amethyst: add event loop which reevaluates the windows a few times after each relevant event. Major hack

--- apps/amethyst.py
+++ apps/amethyst.py
@@ -1,21 +1,25 @@
 import os
 import json
+import time
 
 from talon import ui, resource
-from talon.voice import Key, Context
+from talon.voice import Key, Context, press
 
 from .. import utils
 
-ctx = Context(
-    "amethyst", func=lambda app, win: bool(ui.apps(bundle="com.amethyst.Amethyst"))
-)
+
+def amethyst_running():
+    return bool(ui.apps(bundle="com.amethyst.Amethyst"))
+
+
+ctx = Context("amethyst", func=lambda app, win: amethyst_running())
 
 keymap = {
     # "window next screen": Key("ctrl-alt-shift-l"),
     # "window (previous|prev) screen": Key("ctrl-alt-shift-h"),
     "window next": Key("alt-shift-j"),
     "window previous": Key("alt-shift-k"),
-    "window move desk": Key("ctrl-alt-shift-h"),
+    # "window move desk": Key("ctrl-alt-shift-h"),
     "window full": Key("alt-shift-d"),
     "window tall": Key("alt-shift-a"),
     "window middle": Key("alt-shift-`"),
@@ -34,3 +38,28 @@ keymap.update(
 )
 
 ctx.keymap(keymap)
+
+
+def ui_event(event, arg):
+    if amethyst_running():
+        if event in (
+            "app_activate",
+            "app_launch",
+            "app_close",
+            "win_open",
+            "win_close",
+        ):
+            if event in ("win_open", "win_closed"):
+                if arg.app.name == "Amethyst" or arg.name == "loginwindow":
+                    return
+            if event == "app_activate" and arg.name == "loginwindow":
+                return
+            print(event, arg)
+            press("alt-shift-z")
+            time.sleep(0.25)
+            press("alt-shift-z")
+            time.sleep(0.25)
+            press("alt-shift-z")
+
+
+ui.register("", ui_event)
