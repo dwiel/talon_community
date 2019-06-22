c02470a3cf267ace499649275532ffed5f2f1c96
Zach Dwiel <zdwiel@gmail.com>
2019-06-21 21:23:58 -0400

    add vocab

---
+++ vocab.py
@@ -0,0 +1,12 @@
+from talon.voice import Context
+
+from . import config
+from .text import shrink
+
+vocab_alternate = config.load_config_json("vocab_alternate.json", dict)
+
+vocab_alternate.update({f"shrink {k}": v for k, v in shrink.shrink_map.items()})
+
+ctx = Context("vocab")
+ctx.vocab = config.load_config_json("vocab.json", list) + list(vocab_alternate.keys())
+ctx.vocab_remove = config.load_config_json("vocab_remove.json", dict)
