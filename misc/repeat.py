4178c1be6ed36a3cdc7550a8db651af605a4abb2
John Cooper <jcooper@korgrd.com>
2019-06-26 17:41:42 -0700

    #174 - revise repeater wink/soup/trace/quarr/fypes/repple to match original voicecode spec. add comments

--- misc/repeat.py
+++ misc/repeat.py
@@ -1,5 +1,18 @@
 # originally from https://github.com/JonathanNickerson/talon_voice_user_scripts
 # and https://github.com/pimentel/talon_user/blob/master/repeat.py
+#
+# based on the original voicecode spec https://voicecode.io/doc/repetition:
+#   "hello repple two" types `hellohello`
+#   "shock repple five" presses `return` key 5 times
+#   "hello soup" is equivalent to "hello repple two"
+#   "hello trace" is equivalent to "hello repple three"
+#
+#   Note, "hello repple one" or "hello wink" will also output hellohello, as in this case it
+#   means 'repeat previous once more', whereas every parameter more than 1 means 'repeat previous N times in total'.
+#
+#   Creek repeats the entire previous phrase.
+#   "hello world clamor trace" types "hello world!!!"
+#   Then saying "shock creek" will type a newline followed by "hello world!!!" again
 
 from talon.voice import Context, Rep, RepPhrase, talon
 from .. import utils
@@ -13,8 +26,10 @@ def repeat(m):
     #         * Prevent stacking of repetitions upon previous repetitions
     repeat_count = utils.extract_num_from_m(m)
 
-    if repeat_count is not None and repeat_count >= 2:
-        repeater = Rep(repeat_count - 1)
+    if repeat_count is not None:
+        if repeat_count >= 2:
+            repeat_count -= 1
+        repeater = Rep(repeat_count)
         repeater.ctx = talon
         return repeater(None)
 
@@ -23,10 +38,10 @@ ctx.keymap(
     {
         "wink": Rep(1),
         "creek": RepPhrase(1),
-        "soup": Rep(2),
-        "trace": Rep(3),
-        "quarr": Rep(4),
-        "fypes": Rep(5),
+        "soup": Rep(1),
+        "trace": Rep(2),
+        "quarr": Rep(3),
+        "fypes": Rep(4),
         "(repeat | repple)" + utils.numerals: repeat,
     }
 )
