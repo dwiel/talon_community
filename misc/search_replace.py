36227eef8102cb2f70501ee3ab8e0f43b5fc6bce
Zach Dwiel <zdwiel@gmail.com>
2019-10-03 16:38:54 -0400

    marthis: search for the currently highlighted text if there is highlighted text

--- misc/search_replace.py
+++ misc/search_replace.py
@@ -1,13 +1,27 @@
-from talon.voice import Context, Key
-from ..utils import text
+from talon.voice import Context, Key, press
+from .. import utils
 
 ctx = Context("search_replace")
 
+
+def marthis(m):
+    selected = utils.copy_selected()
+    if selected:
+        press("cmd-f")
+        utils.paste_text(selected)
+        press("enter")
+    else:
+        press("alt-right")
+        press("shift-alt-left")
+        press("cmd-f")
+        press("enter")
+
+
 keymap = {
-    "(search | marco) [<dgndictation>] [over]": [Key("cmd-f"), text, Key("enter")],
+    "marco [<dgndictation>] [over]": [Key("cmd-f"), utils.text, Key("enter")],
     "marneck": Key("cmd-g"),
     "marpreev": Key("cmd-shift-g"),
-    "marthis": [Key("alt-right"), Key("shift-alt-left"), Key("cmd-f"), Key("enter")],
+    "marthis": marthis,
     "(find selected text | find selection | sell find)": Key("cmd-e cmd-f enter"),
     "set selection [text]": Key("cmd-e"),
     "set replacement [text]": Key("cmd-shift-e"),
