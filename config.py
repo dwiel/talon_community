00d434bdff20044d72601b078d0f777ff4e65a11
Zach Dwiel <zdwiel@gmail.com>
2019-10-03 15:50:22 -0400

    fix config

--- config.py
+++ config.py
@@ -10,11 +10,13 @@ except Exception:
 
 
 def load_config_json(filename, default=dict):
-    if not os.path.isfile(filename):
+    try:
+        f = resource.open(filename, "r")
+        f.close()
+    except FileNotFoundError:
+        print("creating missing resource file ", filename)
         with resource.open(filename, "w") as f:
-            print('creating file', filename)
             f.write(json.dumps(default()))
-
     try:
         return json.load(resource.open(filename))
     except Exception as e:
