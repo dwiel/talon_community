d8cadbaa49c9089597fb25f65474ab24f0b80da0
Zach Dwiel <zdwiel@gmail.com>
2019-11-01 10:16:24 -0400

    add notes for making jupyter notebooks work better

--- apps/web/jupyter.py
+++ apps/web/jupyter.py
@@ -1,3 +1,24 @@
+"""
+https://github.com/jupyter/notebook/issues/3400
+
+%%javascript
+var cell = Jupyter.notebook.get_selected_cell();
+var config = cell.config;
+var patch = {
+      CodeCell:{
+        cm_config:{lineWiseCopyCut:false}
+      }
+    }
+config.update(patch)
+
+https://stackoverflow.com/questions/22843891/turn-off-auto-closing-parentheses-in-ipython/
+
+from notebook.services.config import ConfigManager
+c = ConfigManager()
+c.update('notebook', {"CodeCell": {"cm_config": {"autoCloseBrackets": False}}})
+"""
+
+
 from talon.voice import Context, Key
 
 ctx = Context("jupyter")
